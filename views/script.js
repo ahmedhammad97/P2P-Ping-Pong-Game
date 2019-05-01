8482f7d2ec0c67ac459625104892010239fd4fcf
AhmedHammad <hammad97official@gmail.com>
2019-05-01 02:45:24 +0200

    Add ball functions and collision functions

--- views/script.js
+++ views/script.js
@@ -3,16 +3,20 @@ var PositionY = 0;
 var canvas = document.querySelector("#gameTable");
 var msgBar = document.querySelector("#message");
 var myPaddle, otherPaddle = null;
+var ball = null;
 var transimission = null;
+var paddleCollide = false;
 
 socket.onmessage = message => {
   handleMessage(JSON.parse(message.data))
 }
 
 socket.onclose = () => {
-    msgBar.innerHTML = "Connection lost!"
-    clearInterval(transimission)
+  msgBar.innerHTML = "Connection lost!"
+  clearInterval(transimission)
 };
+
+drawTableLines()
 
 function handleMessage(message){
   switch (message.type) {
@@ -33,7 +37,7 @@ function handleMessage(message){
       break;
 
     case "position":
-      updateOtherPos(message)
+      updatePaddle(otherPaddle, otherPaddle.xPos, message.value)
       break;
     default:
 
@@ -42,36 +46,40 @@ function handleMessage(message){
 
 function prepareGameView(message){
   leftPaddle = {
+    "side" : "left",
     "paddle" : canvas.getContext("2d"),
     "xPos" : 10,
     "yPos" : 200
   }
   rightPaddle = {
+    "side" : "right",
     "paddle" : canvas.getContext("2d"),
     "xPos" : 785,
     "yPos" : 200
   }
 
   drawPaddles(leftPaddle, rightPaddle)
+
   if (message.pos === "right"){myPaddle = rightPaddle; otherPaddle = leftPaddle;}
   else {myPaddle = leftPaddle; otherPaddle = rightPaddle;}
 
+  ball = {
+    "shape" : canvas.getContext("2d"),
+    "xPos" : 397,
+    "yPos" : 247,
+    "yVel" : 5,
+    "xVel" : 5
+  }
+
+  setInterval(ballUpdate, transimissionRate)
 }
 
 function trackMouse(){
   canvas.addEventListener("mousemove", event => {
     PositionY = event.clientY - canvas.offsetTop - 50;
     // Move game object
-    myPaddle.paddle.clearRect(myPaddle.xPos, myPaddle.yPos, 5, 100)
-    myPaddle.paddle.fillRect(myPaddle.xPos, PositionY, 5, 100)
-    myPaddle.yPos = PositionY
+    updatePaddle(myPaddle, myPaddle.xPos, PositionY)
   })
-}
-
-function updateOtherPos(message){
-  myPaddle.paddle.clearRect(otherPaddle.xPos, otherPaddle.yPos, 5, 100)
-  myPaddle.paddle.fillRect(otherPaddle.xPos, message.value, 5, 100)
-  otherPaddle.yPos = message.value
 }
 
 function beginTransimission(){
@@ -80,16 +88,73 @@ function beginTransimission(){
   } ,transimissionRate)
 }
 
-(function drawTableLines(){
+function drawTableLines(){
   let line = canvas.getContext("2d")
   line.fillStyle = "#FFFFFF";
   line.fillRect(398, 0, 4, 500);
-})()
+}
 
 function drawPaddles(leftPaddle, rightPaddle){
-  leftPaddle.paddle.fillStyle = "FFFFFF"
+  leftPaddle.paddle.fillStyle = "#FFFFFF"
   leftPaddle.paddle.fillRect(10, 200, 5, 100)
 
-  rightPaddle.paddle.fillStyle = "FFFFFF"
+  rightPaddle.paddle.fillStyle = "#FFFFFF"
   rightPaddle.paddle.fillRect(785, 200, 5, 100)
+}
+
+function updatePaddle(paddle, x, y){
+  paddle.paddle.clearRect(paddle.xPos, paddle.yPos, 5, 100)
+  paddle.paddle.fillRect(x, y, 5, 100)
+  paddle.xPos = x
+  paddle.yPos = y
+}
+
+function ballUpdate(){
+  clearBall()
+
+  ball.xPos += ball.xVel;
+  ball.yPos += ball.yVel;
+
+  if(collideTop() || collideBottom()){ball.yVel = -ball.yVel}
+  if(collidePaddles()){ball.xVel = -ball.xVel; console.log("Paddle Hit!");}
+  if(collideLeft()){msgBar.innerHTML = "Left player scored a goal!"; ball.xVel = -ball.xVel}
+  if(collideRight()){msgBar.innerHTML = "Right player scored a goal!"; ball.xVel = -ball.xVel}
+  if(nearCenter()){drawTableLines()}
+
+  renderBall()
+}
+
+function collideTop(){return ball.yPos <= 0}
+function collideBottom(){return ball.yPos + 6 >= 500}
+function collideLeft(){return ball.xPos <= 0}
+function collideRight(){return ball.xPos + 6 >= 800}
+function collidePaddles(){
+  if(paddleCollide){return false;}
+  let myPaddleTop = myPaddle.yPos
+  let myPaddleBottom = myPaddleTop + 100
+  let otherTop = otherPaddle.yPos
+  let otherBottom = otherTop + 100
+  if(myPaddle.side === "left"){
+    if(ball.xVel < 0 && ball.xPos <= 20 && ball.yPos >= myPaddleTop && ball.yPos <= myPaddleBottom){setPaddleCollide(); return true;}
+    else if(ball.xVel > 0 && ball.xPos + 26 >= 800 && ball.yPos >= otherTop && ball.yPos <= otherBottom){setPaddleCollide(); return true;}
+  }
+  else{
+    if(ball.xVel < 0 && ball.xPos <= 20 && ball.yPos >= otherTop && ball.yPos <= otherBottom){setPaddleCollide(); return true;}
+    else if(ball.xVel > 0 && ball.xPos + 26 >= 800 && ball.yPos >= myPaddleTop && ball.yPos <= myPaddleBottom){setPaddleCollide(); return true;}
+  }
+  return false
+}
+function nearCenter(){return ball.xPos > 350 && ball.xPos < 450}
+
+function setPaddleCollide(){
+  paddleCollide = true;
+  setTimeout(() => {paddleCollide = false;}, 500)
+}
+
+function clearBall(){
+  ball.shape.clearRect(ball.xPos, ball.yPos, 6, 6)
+}
+
+function renderBall(){
+  ball.shape.fillRect(ball.xPos, ball.yPos, 6, 6)
 }
